e0a97c148a2545c7705d566d05dcf8dba65a8b2a
kazu <kazu8160@gmail.com>
2019-11-13 02:18:18 +0900

    Relay Specificationに則ったschemaに変更

--- functions/src/graphql/schema.ts
+++ functions/src/graphql/schema.ts
@@ -1,24 +1,51 @@
 import { gql } from 'apollo-server-express'
 
 const mockUsers = [
-  { name: 'foo', age: 10 },
-  { name: 'bar', age: 100}
+  { id: 1, name: 'foo', age: 10 },
+  { id: 2, name: 'bar', age: 100 }
 ]
 
+// Schema設計はRelay Specificationに則る
+// see: https://relay.dev/docs/en/v6.0.0/graphql-server-specification
 export const typeDefs = gql`
-  type Query {
-    users: [User!]!
+  interface Node {
+    id: ID!
   }
 
-  type User {
+  type PageInfo {
+    hasNextPage: Boolean!
+    hasPreviousPage: Boolean!
+    startCursor: String
+    endCursor: String
+  }
+
+  type Query {
+    users(first: Int!): UserConnection!
+  }
+
+  type User implements Node {
+    id: ID!
     name: String!
     age: Int!
+  }
+  
+  type UserEdge {
+    node: User!
+    cursor: String!
+  }
+
+  type UserConnection {
+    edges: [UserEdge!]!
+    pageInfo: PageInfo!
   }
 `
 
 export const resolvers = {
   Query: {
-    users: () => mockUsers,
+    users: (_: undefined, { first }: { first: number }) => {
+      // TODO: Relay Specificationに則ったレスポンスを返す
+      return mockUsers[first]
+    },
   },
 }
 
