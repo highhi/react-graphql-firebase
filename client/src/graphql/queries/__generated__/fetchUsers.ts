b579e4c3610951ee04170cec3e1bef948af49dff
kazu <kazu8160@gmail.com>
2019-11-12 22:53:35 +0900

    型を生成

---
+++ client/src/graphql/queries/__generated__/fetchUsers.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: fetchUsers
+// ====================================================
+
+export interface fetchUsers_users {
+  __typename: "User";
+  name: string | null;
+}
+
+export interface fetchUsers {
+  users: (fetchUsers_users | null)[] | null;
+}
